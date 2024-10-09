077bb317e0175aadd09b64b82282537a221fedab
will Farrell <willfarrell@proton.me>
2024-10-09 11:15:23 -0600

    feat: update to use esm and node:test

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
-'use strict';
-
-if (typeof window != 'object') {
-  var glob = require('' + 'glob');
-  var path = require('' + 'path');
-}
-
-
-module.exports = jsonSchemaTest;
-
+import path from 'node:path'
+import { readFileSync } from 'node:fs'
+import { describe, it } from 'node:test'
+import { equal, ok } from 'node:assert'
+import glob from 'glob'
 
 function jsonSchemaTest(validators, opts) {
-  var assert = opts.assert || require('' + 'assert');
-  var _Promise;
+  globalThis.describe ??= opts.describe
+  var _Promise
   if (opts.async) {
-    _Promise = opts.Promise || Promise;
-    if (!_Promise) throw new Error('async mode requires Promise support');
+    _Promise = opts.Promise || Promise
+    if (!_Promise) throw new Error('async mode requires Promise support')
   }
 
-  skipOrOnly(opts, describe)(opts.description || 'JSON schema tests', function() {
-    if (opts.timeout) this.timeout(opts.timeout);
-    for (var suiteName in opts.suites)
-      addTests(suiteName, opts.suites[suiteName]);
-  });
-
+  skipOrOnly(opts, describe)(
+    opts.description || 'JSON schema tests',
+    function () {
+      if (opts.timeout) this.timeout(opts.timeout)
+      for (var suiteName in opts.suites)
+        addTests(suiteName, opts.suites[suiteName])
+    }
+  )
 
   function addTests(suiteName, filesOrPath) {
-    describe(suiteName, function() {
+    describe(suiteName, function () {
       var files = Array.isArray(filesOrPath)
-                  ? filesOrPath
-                  : getTestFiles(filesOrPath);
+        ? filesOrPath
+        : getTestFiles(filesOrPath)
 
       files.forEach(function (file) {
         var filter = {
@@ -36,83 +33,102 @@ function jsonSchemaTest(validators, opts) {
           only: getFileFilter(file, 'only')
         }
 
-        skipOrOnly(filter, describe)(file.name, function() {
+        skipOrOnly(filter, describe)(file.name, function () {
           if (file.test) {
-            var testSets = file.test;
+            var testSets = file.test
           } else if (file.path) {
-            var testPath = file.path
-              , testDir = path.dirname(testPath);
-            var testSets = require(testPath);
+            var testPath = file.path,
+              testDir = path.dirname(testPath)
+            var testSets = JSON.parse(readFileSync(testPath))
           }
           testSets.forEach(function (testSet) {
-            skipOrOnly(testSet, describe)(testSet.description, function() {
+            skipOrOnly(testSet, describe)(testSet.description, function () {
               if (Array.isArray(testSet.schemas))
                 testSet.schemas.forEach(function (schema, i) {
-                  var descr = schema.description || schema.id || schema.$ref || ('#' + i);
-                  describe('schema ' + descr, function() {
-                    testSchema(schema);
-                  });
-                });
-              else
-                testSchema(testSet.schema);
+                  var descr =
+                    schema.description || schema.id || schema.$ref || '#' + i
+                  describe('schema ' + descr, function () {
+                    testSchema(schema)
+                  })
+                })
+              else testSchema(testSet.schema)
 
               function testSchema(schema) {
                 testSet.tests.forEach(function (test) {
-                  skipOrOnly(test, it)(test.description, function() {
+                  skipOrOnly(test, it)(test.description, function () {
                     if (Array.isArray(validators)) {
-                      if (opts.async) return _Promise.all(validators.map(doTest));
-                      else validators.forEach(doTest);
+                      if (opts.async)
+                        return _Promise.all(validators.map(doTest))
+                      else validators.forEach(doTest)
                     } else {
                       return doTest(validators)
                     }
-                  });
+                  })
 
                   function doTest(validator) {
-                    var data;
+                    var data
                     if (test.dataFile) {
-                      var dataFile = path.resolve(testDir || '', test.dataFile);
-                      data = require(dataFile);
+                      var dataFile = path.resolve(testDir || '', test.dataFile)
+                      data = JSON.parse(readFileSync(dataFile))
                     } else {
-                      data = test.data;
+                      data = test.data
                     }
 
-                    var valid = validator.validate(schema, data);
-                    if (opts.async && typeof valid == 'object' && typeof valid.then == 'function') {
+                    var valid = validator.validate(schema, data)
+                    if (
+                      opts.async &&
+                      typeof valid == 'object' &&
+                      typeof valid.then == 'function'
+                    ) {
                       return valid.then(
-                        function(_valid) { testResults(_valid, null) },
-                        function(err) {
-                          if (err.errors) testResults(false, err.errors);
-                          else testException(err);
+                        function (_valid) {
+                          testResults(_valid, null)
+                        },
+                        function (err) {
+                          if (err.errors) testResults(false, err.errors)
+                          else testException(err)
                         }
-                      );
+                      )
                     } else {
-                      testResults(valid, validator.errors);
+                      testResults(valid, validator.errors)
                     }
 
                     function testResults(valid, errors) {
                       if (opts.asyncValid == 'data' && test.valid === true)
-                        valid = valid === data;
-                      var passed = valid === test.valid;
+                        valid = valid === data
+                      var passed = valid === test.valid
                       if (!passed && opts.log !== false)
-                        console.log('result:', valid, '\nexpected: ', test.valid, '\nerrors:', validator.errors);
-                      if (valid) assert(!errors || errors.length == 0);
-                      else assert(errors.length > 0);
+                        console.log(
+                          'result:',
+                          valid,
+                          '\nexpected: ',
+                          test.valid,
+                          '\nerrors:',
+                          validator.errors
+                        )
+                      if (valid) ok(!errors || errors.length == 0)
+                      else ok(errors.length > 0)
 
-                      suiteHooks(passed, valid, errors);
-                      assert.equal(valid, test.valid);
+                      suiteHooks(passed, valid, errors)
+                      equal(valid, test.valid)
                     }
 
                     function testException(err) {
-                      var passed = err.message == test.error;
+                      var passed = err.message == test.error
                       if (!passed && opts.log !== false)
-                        console.log('error:', err.message,
+                        console.log(
+                          'error:',
+                          err.message,
                           '\nexpected: ',
-                          test.valid ? 'valid'
-                            : test.valid === false ? 'invalid'
-                            : 'error ' + test.error);
+                          test.valid
+                            ? 'valid'
+                            : test.valid === false
+                            ? 'invalid'
+                            : 'error ' + test.error
+                        )
 
-                      suiteHooks(passed);
-                      assert.equal(err.message, test.error);
+                      suiteHooks(passed)
+                      assert.equal(err.message, test.error)
                     }
 
                     function suiteHooks(passed, valid, errors) {
@@ -125,42 +141,46 @@ function jsonSchemaTest(validators, opts) {
                         expected: test.valid,
                         expectedError: test.error,
                         errors: errors
-                      };
+                      }
 
-                      if (opts.afterEach) opts.afterEach(result);
-                      if (opts.afterError && !passed) opts.afterError(result);
+                      if (opts.afterEach) opts.afterEach(result)
+                      if (opts.afterError && !passed) opts.afterError(result)
                     }
                   }
-                });
+                })
               }
-            });
-          });
-        });
-      });
-    });
+            })
+          })
+        })
+      })
+    })
 
     function getFileFilter(file, property) {
-      var filter = opts[property];
-      return Array.isArray(filter) && filter.indexOf(file.name) >= 0;
+      var filter = opts[property]
+      return Array.isArray(filter) && filter.indexOf(file.name) >= 0
     }
   }
 
-
   function skipOrOnly(filter, func) {
-    return filter.only === true ? func.only : filter.skip === true ? func.skip : func;
+    return filter.only === true
+      ? func.only
+      : filter.skip === true
+      ? func.skip
+      : func
   }
 
-
   function getTestFiles(testsPath) {
-    var files = glob.sync(testsPath, { cwd: opts.cwd });
+    var files = glob.sync(testsPath, { cwd: opts.cwd })
     return files.map(function (file) {
       var match = file.match(/([\w\-_]+\/)[\w\-_]+\.json/)
-      var folder = match ? match[1] : '';
-      if (opts.hideFolder && folder == opts.hideFolder) folder = '';
+      var folder = match ? match[1] : ''
+      if (opts.hideFolder && folder == opts.hideFolder) folder = ''
       return {
         path: path.join(opts.cwd, file),
         name: folder + path.basename(file, '.json')
-      };
-    });
+      }
+    })
   }
 }
+
+export default jsonSchemaTest
